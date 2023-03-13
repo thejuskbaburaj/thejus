7a4c5e8a582d6bc2feb05a17a1e5e34b735db5c3
Akashkp-01 <akashpushpan7@gmail.com>
2023-03-13 03:09:49 -0700

    khde

---
+++ script.js
@@ -0,0 +1,93 @@
+// Datatypes
+
+// var x = 20;
+// {
+//     var x=10;
+//     console.log(x);
+// }
+// console.log(y);
+// const pi =3.14;
+// console.log(pi);
+// var sum=5+10;
+// console.log(sum);
+// var mul=5*10;
+// console.log(mul);
+// var sub=5-10;
+// console.log(sub);
+// var x = 10;
+// var y = 5
+// var sum = x+y;
+// console.log(sum);
+// documents.write("sum ="+ sum);
+// documents.write("mul ="+ mul);
+// documents.write("sub ="+ sub);
+// documents.write("sum ")
+// var fruits =["apple","banana","orange",10]
+// console.log(fruits[0]);
+// console.log(fruits.length);
+// fruits.push("plum");
+// fruits.unshift("grape")
+// fruits.pop();
+// fruits.shift();
+// console.log(fruits);
+// var person =[
+//     {
+//         name:"thejus",
+//         age:19,
+//         place:"chiyyaram"
+//     },
+//     {
+//         name:"ajas",
+//         age:20,
+//         place:"kochi"
+
+//     }
+// ]
+// console.log(person[1].name);
+// function add(a,b){
+//     var c=a+b;
+
+//     console.log(c)
+// }
+// add(1,2)
+// add(2,3)
+
+// const addition =(a,b)=>{
+//     var c = a+b;
+
+//     console.log(c)
+
+// }
+// addition(1,2)
+// const mul=(a,b)=>{
+//     var c=a*b;
+// var x= 20;
+// if(x>20){
+//     console.log("good");
+// }
+// else
+// console.log("bad");
+
+
+//     console.log(c)
+// }
+// mul(1,2)
+for(var i=1;i<5;i++)
+{
+    console.log(i);
+}
+var fruits =["apple","banana","orange","karikku",10];
+for(let i=0;i<fruits.length;i++){
+    console.log(fruits[i])
+}
+
+let p=0;
+while(p<fruits.length){
+    console.log(fruits[p]);
+    p++;
+}
+let i=0;
+do{
+    console.log(fruits{i}):
+    i++
+}while(i<fruits.length)
